55d53dd93ed5c33a156d923fcc9c9cf39e6aae67
geibriel12 <marquezzd4@gmail.com>
2026-01-03 18:07:08 -0300

    alteração no server.mjs erro p2002

--- server.mjs
+++ server.mjs
@@ -1,4 +1,4 @@
- import 'dotenv/config'; 
+import 'dotenv/config'; 
 import express from 'express';
 // Importe 'Prisma' junto com 'PrismaClient'
 import { PrismaClient, Prisma } from '@prisma/client'; 
