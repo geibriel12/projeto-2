b6fd7b82fd1c35c22ccd15818bfc716464c12a34
geibriel12 <marquezzd4@gmail.com>
2026-01-04 18:24:18 -0300

    hbjnj

--- server.mjs
+++ server.mjs
@@ -96,7 +96,7 @@ app.delete('/api/usuarios/:id', async (req, res) => {
 });
 
 // Rota coringa
-app.get("(.*)", (req, res) => {
+app.get("/:any*", (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
